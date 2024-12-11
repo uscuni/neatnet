1b7cd9f6140d46ffca06f6d566f3360046c476c9
James Gaboardi <jgaboardi@gmail.com>
2024-12-11 09:13:25 -0500

    initial try

--- neatnet/simplify.py
+++ neatnet/simplify.py
@@ -96,6 +96,36 @@ def _identify_non_planar(
     return artifacts
 
 
+def classification_sequence(
+    roads, artifacts, compute_coins, eps: float = 1e-4, step="singletons"
+):
+    """..."""
+
+    # Extract network nodes and relate to artifacts
+    nodes, artifacts = _link_nodes_artifacts(step, roads, artifacts, eps)
+
+    # Compute number of stroke groups per artifact
+    if compute_coins:
+        roads, _ = continuity(roads)
+
+    artifacts = _classify_strokes(artifacts, roads)
+
+    # Filter artifacts caused by non-planar intersections
+    artifacts = _identify_non_planar(artifacts, roads)
+
+    # Count intersititial nodes (primes)
+    _prime_count = artifacts["node_count"] - artifacts[["C", "E", "S"]].sum(axis=1)
+    artifacts["interstitial_nodes"] = _prime_count
+
+    # Define the type label
+    ces_type = []
+    for x in artifacts[["node_count", "C", "E", "S"]].itertuples():
+        ces_type.append(f"{x.node_count}{'C' * x.C}{'E' * x.E}{'S' * x.S}")
+    artifacts["ces_type"] = ces_type
+
+    return nodes, artifacts, roads
+
+
 def simplify_singletons(
     artifacts: gpd.GeoDataFrame,
     roads: gpd.GeoDataFrame,
@@ -159,12 +189,14 @@ def simplify_singletons(
         The road network line data following the singleton procedure.
     """
 
+    """
     # Extract network nodes and relate to artifacts
     nodes, artifacts = _link_nodes_artifacts("singletons", roads, artifacts, eps)
 
     # Compute number of stroke groups per artifact
     if compute_coins:
         roads, _ = continuity(roads)
+
     artifacts = _classify_strokes(artifacts, roads)
 
     # Filter artifacts caused by non-planar intersections
@@ -179,6 +211,12 @@ def simplify_singletons(
     for x in artifacts[["node_count", "C", "E", "S"]].itertuples():
         ces_type.append(f"{x.node_count}{'C' * x.C}{'E' * x.E}{'S' * x.S}")
     artifacts["ces_type"] = ces_type
+    """
+
+    # Classify artifact typology
+    nodes, artifacts, roads = classification_sequence(
+        roads, artifacts, compute_coins, eps=eps
+    )
 
     # Collect changes
     to_drop: list[int] = []
